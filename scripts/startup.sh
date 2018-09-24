1995ffb315ec1410438cc1b010c2ed6f21e8944e
Roman <roman_buta@epam.com>
2018-09-24 16:50:38 +0300

    config.yaml

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,4 +3,5 @@ set -e
 yum -y install git nginx
 git clone https://github.com/kolakej/DatadogBootstrap.git
 chmod 755 DatadogBootstrap/bootstrap.sh
-bash DatadogBootstrap/bootstrap.sh --agent= --integration=nginx 
+cd DatadogBootstrap
+bash ./bootstrap.sh --agent= --integration=nginx 
